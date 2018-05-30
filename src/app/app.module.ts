7c3ba024e03cf3d739cdc6ef22b5acc44673ee54
David Martín Pérez <davidmartinperez1@gmail.com>
2018-05-30 17:09:27 +0200

    Creando mi primer componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Aquí se importan las clases de los componentes
 import { AppComponent } from './app.component';
+import { VideojuegosComponent } from './videojuego/videojuego.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // De claramos los compoenentes
+    AppComponent,
+    VideojuegosComponent
   ],
   imports: [
     BrowserModule
